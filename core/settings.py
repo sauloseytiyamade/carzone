8844ff7318b9f6544847785898b8ee2ca0d56eca
Saulo Yamade <sauloseytiyamade@gmail.com>
2022-09-04 22:45:23 -0300

    Course finish

--- core/settings.py
+++ core/settings.py
@@ -27,12 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'dashboard'
+
 
 # Application definition
 
 INSTALLED_APPS = [
     'pages',
     'cars',
+    'accounts',
+    'contacts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    #providers
+
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +148,19 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+SITE_ID = 1
+
+# Email sending
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sauloseytiyamade@gmail.com'
+EMAIL_HOST_PASSWORD = 'meqsfwqwcgcjwnpf'
+EMAIL_USE_TLS = True
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from pages.models import Team
 from cars.models import Car
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -38,4 +41,28 @@ def services(request):
     return render(request, 'pages/services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        phone = request.POST['phone']
+        message = request.POST['message']
+
+        message_body = 'Name:' + name + '. Email: ' + email + '. Phone: ' + phone + '. Message: ' + message
+        email_subject = 'You have a new message from Carzone website regarding ' + subject
+
+        admin_info = User.objects.get(is_superuser=True)
+        admin_email = admin_info.email
+
+        send_mail(
+            email_subject,
+            message_body,
+            'sauloseytiyamade@gmail.com',
+            [admin_email],
+            fail_silently=False,
+        )
+
+        messages.success(request, 'Thank you for contacting us. We will get back to you shortly. ')
+        return redirect('contact')
+
     return render(request, 'pages/contact.html')
